9e2823536aa31860f13bc15bf9cd698d03901dff
Sergei Andruhovich <sergey_andr1998@mail.ru>
2020-04-16 20:36:57 +0300

    Add bad controler

---
+++ alcoholmarket/src/main/java/com/am/controler/LiqueurServlet.java
@@ -0,0 +1,33 @@
+package com.am.controler;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/controler")
+public class LiqueurServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        service(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        service(req, resp);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        String newResponce = req.getParameter("page");
+        String url = "/" + newResponce + ".jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, res);
+    }
+}
