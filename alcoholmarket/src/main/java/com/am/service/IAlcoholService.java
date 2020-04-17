2bf7c9220b0495498997105a249c419dcd4c7995
017zontik <017zontik@gmail.com>
2020-04-17 12:51:05 +0300

    AlcoholService has been added

---
+++ alcoholmarket/src/main/java/com/am/service/IAlcoholService.java
@@ -0,0 +1,12 @@
+package com.am.service;
+
+import com.am.entity.AlcoholDrink;
+
+public interface IAlcoholService {
+    List<AlcoholDrink> getList();
+    void addAlcoholDrink(AlcoholDrink newAlcohol);
+   // void delAlcohol(Alcohol dellAlcohol);
+
+
+
+}
